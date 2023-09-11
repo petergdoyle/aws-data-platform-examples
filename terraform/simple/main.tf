9604975c257d7455d720c1c591b88f1472c07ff8
Peter Doyle <peter.g.doyle.pro@gmail.com>
2023-09-11 09:25:23 -0600

    work to date

--- terraform/simple/main.tf
+++ terraform/simple/main.tf
@@ -1,1 +1,102 @@
 # main 
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.current.user_id
+}
+
+resource "aws_s3_bucket" "s3-bucket" {
+  bucket = "${var.bucket_name}"
+  force_destroy = true
+}  
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "pb" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.ownership]
+  bucket = aws_s3_bucket.s3-bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "folder1" {
+  bucket = aws_s3_bucket.s3-bucket.bucket
+  key    = "results/"
+  source = "/dev/null"
+  acl    = "private"
+}
+
+resource "aws_s3_object" "folder2" {
+  bucket = aws_s3_bucket.s3-bucket.bucket
+  key    = "uploads/"
+  source = "/dev/null"
+  acl    = "private"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.func.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.bucket.arn
+}
+
+resource "aws_lambda_function" "func" {
+  filename      = "your-function.zip"
+  function_name = "example_lambda_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "exports.example"
+  runtime       = "go1.x"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "your-bucket-name"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.func.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+    filter_suffix       = ".log"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
--- terraform/simple/variables.tf
+++ terraform/simple/variables.tf
@@ -1,1 +1,6 @@
-# variables  
+# variables.tf – The variables.tf file allows you to define the variables  
+
+variable "bucket_name" {
+    description = "The name of the s3 bucket to create."
+    # default = ["767557550016-aws-data-platform-examples-"]
+}
